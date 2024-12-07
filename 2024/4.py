7cb746c23f9a1d2e4fd4c4fcc4c5c94a82abca6c
Teofil-Constantin Ciobanu <tciobanu.dev@gmail.com>
2024-12-07 17:28:02 +0200

    2024 4b

--- 2024/4.py
+++ 2024/4.py
@@ -4,37 +4,33 @@ with open('inputs\\4') as fd:
     content = fd.read()
 
 m = content.split('\n')
-needle = "XMAS"
+needle = "MAS"
 
-def search_pos_dir(_pos, dir):
-    pos = _pos.copy()
-    for character in needle:
-        if not 0 <= pos[1] < len(m):
-            return False
-        if not 0 <= pos[0] < len(m[0]):
-            return False
-        if m[pos[1]][pos[0]] != character:
-            return False
-        pos += dir
+def posInside(pos):
+    if not 0 <= pos[1] < len(m):
+        return False
+    if not 0 <= pos[0] < len(m[0]):
+        return False
     return True
 
-dirs = [
-    np.array([0, 1]),
-    np.array([1, 1]),
-    np.array([1, 0]),
-    np.array([1, -1]),
-    np.array([0, -1]),
-    np.array([-1, -1]),
-    np.array([-1, 0]),
-    np.array([-1, 1]),
-]
+
+def diag(pos, dir):
+    word = ""
+    for i in range(-1, 2):
+        ipos = pos + dir * i
+        if not posInside(ipos):
+            return False
+        word += m[ipos[1]][ipos[0]]
+    if word == needle or word[::-1] == needle:
+        return True
+    return False
 res = 0
+dir1 = np.array([1,1])
+dir2 = np.array([-1,1])
 for y in range(0, len(m)):
     for x in range(0, len(m[0])):
         pos = np.array([x,y])
-        for dir in dirs:
-            if search_pos_dir(pos, dir):
-                res += 1
-
+        if diag(pos, dir1) and diag(pos, dir2):
+            res += 1
 print(res)            
 
