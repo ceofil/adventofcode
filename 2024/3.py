b6a7071ac204d2689e8ff2133c23f9e86d56a911
Teofil-Constantin Ciobanu <tciobanu.dev@gmail.com>
2024-12-07 16:43:04 +0200

    2024 3b

--- 2024/3.py
+++ 2024/3.py
@@ -2,14 +2,23 @@ import re
 
 
 
+do_regex = re.compile(r"don\'t\(\)|do\(\)")
 
-reg = r"mul\(([0-9]+,[0-9]+)\)"
-creg = re.compile(reg)
-with open('inputs\\3b.test') as fd:
+with open('inputs\\3') as fd:
     content = fd.read()
-mulstrings = creg.findall(content)
+content = "do()" + content
+s = re.split(do_regex, content)
+s = s[1:]
+sep = re.findall(do_regex, content)
+
 res = 0
-for mulstring in mulstrings:
-    a,b = mulstring.split(',')
-    res += int(a) * int(b)
+compiled_mul_regex = re.compile(r"mul\(([0-9]+,[0-9]+)\)")
+for string, separator in zip(s, sep, strict=True):
+    if separator == "don't()":
+        continue
+    mul_regex = r"mul\(([0-9]+,[0-9]+)\)"
+    mulstrings = compiled_mul_regex.findall(string)
+    for mulstring in mulstrings:
+        a,b = mulstring.split(',')
+        res += int(a) * int(b)
 print(res)
