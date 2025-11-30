58b3340de2182afd2d62d88a8484661bf4ed4367
Teofil-Constantin Ciobanu <tciobanu.dev@gmail.com>
2025-11-30 16:55:05 +0200

    ??? forgot to push

--- 2024/9.py
+++ 2024/9.py
@@ -1,41 +1,29 @@
 
 
-with open("inputs/9", "r") as fd:
+with open("inputs/9.test", "r") as fd:
     c = fd.read()
 
 c = list(map(int, c))
 lens = c[::2]
 spaces = c[1::2]
+if len(spaces) < len(lens):
+    spaces.append(0)
 print(lens)
 print(spaces)
 
-res = 0
-idx = 0
-LEFT_ID = 0
-RIGHT_ID = len(lens) - 1
-SPACE_IDX = 0
-from_left = True
 
-finished = False
-while not finished:
-    if from_left:
-        for _ in range(lens[LEFT_ID]):
-            res += idx * LEFT_ID
-            idx += 1
-        lens[LEFT_ID] = 0
-        LEFT_ID += 1
-        from_left = False
-    else:
-        if SPACE_IDX < len(spaces):
-            for _ in range(spaces[SPACE_IDX]):
-                if lens[RIGHT_ID] == 0:
-                    RIGHT_ID -= 1
-                if lens[RIGHT_ID] == 0:
-                    finished = True
-                    break
-                lens[RIGHT_ID] -= 1
-                res += idx * RIGHT_ID
-                idx += 1
-            SPACE_IDX += 1
-        from_left = True
-print(res)
+
+def shiftIdxToLeft(lens: list, spaces:list, lenIdx:int, spaceIdx:int):
+    length = lens.pop(lenIdx)
+    space = spaces.pop(lenIdx)
+    spaces[spaceIdx] -= length
+    spaces[lenIdx] += length
+    lens.insert(spaceIdx+1, length)
+    spaces.insert(spaceIdx, 0)
+
+# 2333133121414131402
+
+delta = 0
+shiftIdxToLeft(lens, spaces, lenIdx=-1, spaceIdx=0)
+print(lens)
+print(spaces)
