124ba4362332de4953014d6982c04a78b89438a7
Teofil-Constantin Ciobanu <tciobanu.dev@gmail.com>
2025-12-01 15:45:50 +0200

    2025 1b

--- 2025/1.py
+++ 2025/1.py
@@ -11,9 +11,15 @@ with open('inputs/1', 'r') as fd:
 cursor = 50
 result = 0
 for dirrection, magnitude in data:
+    old_cursor = cursor
     cursor += dirrection * magnitude
+    if cursor <= 0:
+        result += abs(cursor)//100 + 1
+        if old_cursor == 0:
+            result -= 1  # already counted
+    if cursor >= 100:
+        result += cursor//100
+
     cursor = cursor % 100
-    if cursor == 0:
-        result += 1
-    print(cursor)
+
 print("result:", result)
