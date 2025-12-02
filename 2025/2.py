185f091b0c914740d9d6b2fa381bbe0fc09cb700
Teofil-Constantin Ciobanu <tciobanu.dev@gmail.com>
2025-12-02 14:26:09 +0200

    2025 2b

--- 2025/2.py
+++ 2025/2.py
@@ -1,5 +1,5 @@
 import sys
-
+from tqdm import tqdm
 ranges = []
 with open(sys.argv[1], 'r') as file:
     str_ranges = file.read().replace('\n', '').split(',')
@@ -7,13 +7,19 @@ with open(sys.argv[1], 'r') as file:
         start, end = map(int, r.split('-'))
         ranges.append((start, end))
 
+
+
+
 result = 0
-for r in ranges:
+for r in tqdm(ranges):
     for i in range(r[0], r[1] + 1):
         str_i = str(i)
-        if len(str_i) % 2 != 0:
-            continue
-        mid = len(str_i) // 2
-        if str_i[:mid] == str_i[mid:]:
-            result += i
+
+        for substr_len in range(1, len(str_i)//2 + 1):
+            if len(str_i) % substr_len != 0:
+                continue
+            all_substrings = [str_i[j:j+substr_len] for j in range(0, len(str_i), substr_len)]
+            if all(s == all_substrings[0] for s in all_substrings):
+                result += i
+                break
 print("result:", result)
