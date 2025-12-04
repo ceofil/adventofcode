50244502cec843c238e27665eebfac35ffeaac8a
Teofil-Constantin Ciobanu <tciobanu.dev@gmail.com>
2025-12-04 18:14:43 +0200

    2025 4b

--- 2025/4.py
+++ 2025/4.py
@@ -9,15 +9,22 @@ with open(sys.argv[1], 'r') as file:
 data = np.array(data)
 
 result = 0
-for y in range(data.shape[0]):
-    for x in range(data.shape[1]):
-        if data[y, x] == 0:
-            continue
-        nbh = data[
-            max(0, y-1):min(data.shape[0], y+2), 
-            max(0, x-1):min(data.shape[1], x+2)
-        ].sum() - 1
-        if nbh < 4:
-            result += 1
+
+removed = -1
+while removed != 0:
+    removed = 0
+    for y in range(data.shape[0]):
+        for x in range(data.shape[1]):
+            if data[y, x] == 0:
+                continue
+            nbh = data[
+                max(0, y-1):min(data.shape[0], y+2), 
+                max(0, x-1):min(data.shape[1], x+2)
+            ].sum() - 1
+            if nbh < 4:
+                result += 1
+                removed += 1
+                data[y, x] = 0
+    print("removed:", removed)
 print("result:", result)
         
