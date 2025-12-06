cd58edc20cafd441ca51403321d5772a9899c2dd
Teofil-Constantin Ciobanu <tciobanu.dev@gmail.com>
2025-12-06 14:55:10 +0200

    2025 6b

--- 2025/6.py
+++ 2025/6.py
@@ -3,11 +3,24 @@ import numpy as np
 
 result = 0
 with open(sys.argv[1], 'r') as file:
-    lines = map(lambda l: l.split(), file.read().splitlines())
-for *numbers_str, op in zip(*lines):
-    numbers = list(map(int, numbers_str))
-    if op == '+':
-        result += sum(numbers)
-    elif op == '*':
-        result += np.prod(numbers)
+    lines = file.read().splitlines()
+    transposed = list(zip(*lines))
+
+temp_result = 0
+temp_op = ''
+for *line, op in transposed:
+    clean_line = ''.join(line).strip()
+    if clean_line == '':
+        result += temp_result
+        print("temp_result", temp_result)
+        continue
+    number = int(clean_line)
+    if op != ' ':
+        temp_op = op
+        temp_result = 1 if op == '*' else 0
+    if temp_op == '+':
+        temp_result += number
+    else:
+        temp_result *= number
+result += temp_result
 print("result:", result)
