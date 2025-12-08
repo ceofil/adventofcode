f9be46d32f91067defca9e4fd9758090faad1f0b
Teofil-Constantin Ciobanu <tciobanu.dev@gmail.com>
2025-12-08 22:22:36 +0200

    2025 8b

--- 2025/8.py
+++ 2025/8.py
@@ -1,6 +1,6 @@
 import sys
 import numpy as np
-
+from tqdm import tqdm
 
 data = []
 file = sys.argv[1]
@@ -16,33 +16,36 @@ diff = data[:, np.newaxis, :] - data[np.newaxis, :, :]
 dists = np.linalg.norm(diff, axis=-1)
 
 indeces = [(i1, i2) for i1 in range(len(data)) for i2 in range(i1)]
-indeces = sorted(indeces, key=lambda x: dists[x[0], x[1]])[:n_connections]
+indeces = sorted(indeces, key=lambda x: dists[x[0], x[1]])
 
 edges = np.zeros((len(data), len(data)), dtype=int)
 for i1, i2 in indeces:
     edges[i1, i2] = 1
     edges[i2, i1] = 1
 
-visited = set()
+    visited = set()
 
-clusters = []
-for start_node in range(len(data)):
-    if start_node in visited:
-        continue
-    visited.add(start_node)
-    cluster = [start_node]
-    stack = [start_node]
-    while stack:
-        node = stack.pop()
-        neighbors = np.where(edges[node] == 1)[0]
-        for neighbor in neighbors:
-            if neighbor not in visited:
-                visited.add(neighbor)
-                stack.append(neighbor)
-                cluster.append(int(neighbor))
-    clusters.append(cluster)
+    clusters = []
+    for start_node in range(len(data)):
+        if start_node in visited:
+            continue
+        visited.add(start_node)
+        cluster = [start_node]
+        stack = [start_node]
+        while stack:
+            node = stack.pop()
+            neighbors = np.where(edges[node] == 1)[0]
+            for neighbor in neighbors:
+                if neighbor not in visited:
+                    visited.add(neighbor)
+                    stack.append(neighbor)
+                    cluster.append(int(neighbor))
+        clusters.append(cluster)
 
-sizes = [len(c) for c in clusters]
-sizes.sort(reverse=True)
-print(sizes)
-print("result:", np.prod(sizes[:3]))
+    if len(clusters) == 1 and len(clusters[0]) == len(data):
+        print(data.shape, data[i2,0])
+
+        print("result:", data[i1,0] * data[i2,0])
+        break
+    else:
+        print(f'{len(data)-len(clusters)}/{len(data)}')
